11475696b9562ed36d6c559a13026aa55d2c957c
Darko Vucetic <darko.vucetic7@gmail.com>
2019-07-15 01:41:31 +0200

    login protection

--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,22 @@
 
 Auth::routes();
 
-Route::get('/', 'PageController@home');
-Route::get('/home', 'PageController@home');
-Route::get('/index', 'PageController@comingSoon');
-Route::get('/rent_a_car', 'PageController@rent_a_car');
-Route::get('/transfers', 'PageController@transfers');
-Route::get('/apartments', 'PageController@apartments');
-Route::get('/faq', 'PageController@faq');
-Route::get('/about', 'PageController@about');
-Route::get('/contact', 'PageController@contact');
-Route::get('/test', 'PageController@test');
+Route::get('/', 'PageController@comingSoon');
 Route::post('/newsletter', 'PageController@subscribe');
-Route::post('/send-mail', 'PageController@sendMail');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/home', 'PageController@home');
+    Route::get('/index', 'PageController@comingSoon');
+    Route::get('/rent_a_car', 'PageController@rent_a_car');
+    Route::get('/transfers', 'PageController@transfers');
+    Route::get('/apartments', 'PageController@apartments');
+    Route::get('/faq', 'PageController@faq');
+    Route::get('/about', 'PageController@about');
+    Route::get('/contact', 'PageController@contact');
+    Route::get('/test', 'PageController@test');
+
+    Route::post('/send-mail', 'PageController@sendMail');
 
 
-Route::resource('/events', 'EventController');
-
+    Route::resource('/events', 'EventController');
+});
